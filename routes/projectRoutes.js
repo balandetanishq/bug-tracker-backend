ebef9e82595b68e4ff17d6e36e2e0c0b966abad9
tanishq0607 <162088910+tanishq0607@users.noreply.github.com>
2026-01-31 19:53:58 +0530

    Add auth middleware to projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const {
   getProjects,
@@ -8,7 +9,7 @@ const {
 } = require("../controllers/projectController");
 
 router.get("/", getProjects);
-router.post("/", createProject);
+router.post("/", auth, createProject);
 router.delete("/:id", deleteProject);
 
 module.exports = router;
